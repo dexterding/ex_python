98736928863bdd3c58f2f8bb0bcc413ba1cf793d
Administrator <1047414223@qq.com>
2021-01-24 22:03:20 +0800

    第四节：字典的练习使用-补充reverse使用

--- season4_dict.py
+++ season4_dict.py
@@ -20,6 +20,14 @@ print(d5.items())
 d1_sorted_by_key = sorted(d1.items(),key = lambda x:x[0])
 print(d1_sorted_by_key)
 
+# reverse=True倒序,由大至小
+d1_sorted_by_key2 = sorted(d1.items(),key = lambda x:x[0],reverse=True)
+print(d1_sorted_by_key2)
+
+# reverse=False 正序，从小到大
+d1_sorted_by_key2 = sorted(d1.items(),key = lambda x:x[0],reverse=False)
+print(d1_sorted_by_key2)
+
 #value格式不一致时如何排序
 # d1_sorted_by_value = sorted(d1.items(),key = lambda x:x[1])
 # print(d1_sorted_by_value)
