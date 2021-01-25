6ea2364224e8562ad3d2f2cd15555aa683c78772
dexterding <dingyangjian2013@163.com>
2021-01-25 20:07:35 +0800

    第六节：txt的读取与写入、string的处理

--- season6_input&output.py
+++ season6_input&output.py
@@ -9,4 +9,40 @@
 将结果按行输出到文件 out.txt。
 
 """
+import os
+import re
 
+
+with open("D:\webdriver\ex_python\in.txt","r") as f:
+     text = f.read()
+
+def exe_txt(text):
+    text = text.strip() #去除首尾的空格
+    text = text.lower() #切换为小写
+    #去掉符号和换行符
+    text_split = re.split("\n| ",text)
+
+    # 去掉空的元素
+    text_split_not = list(filter(None,text_split))
+
+
+    cou_num = {}
+    num = 0
+    for word in text_split_not:
+        if word not in cou_num:
+            cou_num[word] = 0
+        cou_num[word] += 1
+
+    # 按照词频排序
+    sored_cout = sorted(cou_num.items(),key=lambda x:x[1],reverse=True)
+    type(cou_num)
+    return sored_cout
+
+
+freq_txt = exe_txt(text)
+print(type(freq_txt))
+
+# with open("D:\webdriver\ex_python\out.txt","w") as out:
+#     out.write(freq_txt)
+#     # for wo,va in freq_txt:
+#         # out.write("{} {}\n".format(wo,va))
